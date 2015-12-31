8a120c4e97251912f5ee5898da1e9b7243f30026
Nathanial Hartman <nathanial.hartman@gmail.com>
2015-12-31 03:53:57 -0800

    basic zoom

--- src/main/java/Main.kt
+++ src/main/java/Main.kt
@@ -1,5 +1,7 @@
 package main
 
+import com.google.common.eventbus.EventBus
+import com.google.common.eventbus.Subscribe
 import org.pcollections.TreePVector
 import java.awt.*
 import java.awt.event.WindowAdapter
@@ -10,21 +12,26 @@ import java.util.*
 import javax.swing.JFrame
 import javax.swing.JPanel
 
-class Coords(val x:Int, val y: Int) {
+
+class ZoomEvent(val zoom: Int) {
 
 }
 
-class Planet(val coords: Coords) {
+class GalacticCoordinates(val x:Int, val y: Int) {
 
 }
 
-class Asteroid(val coords: Coords) {
+class Planet(val galacticCoordinates: GalacticCoordinates) {
+
+}
+
+class Asteroid(val galacticCoordinates: GalacticCoordinates) {
 
 }
 
 class SolarSystem(val name: String,
                   val starColor: Color,
-                  val coords: Coords,
+                  val galacticCoordinates: GalacticCoordinates,
                   val planets: TreePVector<Planet>,
                   val asteroids: TreePVector<Asteroid>) {
 
@@ -44,7 +51,7 @@ class Simulation(val systemCount: Int, val planetsRange: ClosedRange<Int>) {
         while(i < systemCount){
             val planets = TreePVector.empty<Planet>()
             val asteroids = TreePVector.empty<Asteroid>()
-            val coords = Coords(Math.floor(random.nextGaussian() * 100 + 500).toInt(), Math.floor(random.nextGaussian() * 100 + 500).toInt())
+            val coords = GalacticCoordinates(Math.floor(random.nextGaussian() * 100 + 500).toInt(), Math.floor(random.nextGaussian() * 100 + 500).toInt())
             val randColor = Color((Math.random() * 255).toInt(), (Math.random() * 255).toInt(), (Math.random() * 255).toInt())
             systems.add(SolarSystem("System " + i, randColor, coords, planets, asteroids))
             i++;
@@ -54,6 +61,8 @@ class Simulation(val systemCount: Int, val planetsRange: ClosedRange<Int>) {
 }
 
 class GalaxyView(val simulation: Simulation) : JPanel() {
+    var zoom = 1.0
+
     init {
         background = Color.white
     }
@@ -61,12 +70,15 @@ class GalaxyView(val simulation: Simulation) : JPanel() {
     override fun paintComponent(g: Graphics?) {
         super.paintComponent(g)
         val g2d = g as Graphics2D
+        println("Paint Me: " + zoom)
         g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON)
         g2d.setRenderingHint(RenderingHints.KEY_RENDERING, RenderingHints.VALUE_RENDER_QUALITY)
+        g2d.scale(zoom, zoom)
+        g2d.translate(-500,-500)
         for(system in simulation.systems){
             g2d.color = system.starColor
             val radius = 1
-            val theCircle = Ellipse2D.Double(system.coords.x.toDouble() - radius, system.coords.y.toDouble() - radius, 2.0 * radius, 2.0 * radius);
+            val theCircle = Ellipse2D.Double(system.galacticCoordinates.x.toDouble() - radius, system.galacticCoordinates.y.toDouble() - radius, 2.0 * radius, 2.0 * radius);
             g2d.fill(theCircle);
         }
     }
@@ -74,24 +86,42 @@ class GalaxyView(val simulation: Simulation) : JPanel() {
     override fun getPreferredSize(): Dimension? {
         return Dimension(1000,1000)
     }
-}
 
-class SimulationView(val simulation: Simulation) : JPanel() {
-    init {
-        background = Color.white
-        add(GalaxyView(simulation))
+    @Subscribe
+    public fun onZoom(e: ZoomEvent){
+        if(e.zoom > 0){
+            zoom *= 0.9
+        } else {
+            zoom *= 1.1
+        }
+        println("Change Zoom: " +  zoom)
+        repaint()
     }
 }
 
 
-class SimulationFrame(val simulation: Simulation) : JFrame("Galactic Simulation") {
+class SimulationView(val simulation: Simulation, val bus: EventBus) : JPanel() {
+    init {
+        background = Color.white
+        val galaxyView = GalaxyView(simulation)
+        bus.register(galaxyView)
+        add(galaxyView)
+        addMouseWheelListener {
+            val zoom = it.wheelRotation * it.scrollAmount
+            bus.post(ZoomEvent(zoom))
+        }
+    }
+}
+
+
+class SimulationFrame(val simulation: Simulation, val bus: EventBus) : JFrame("Galactic Simulation") {
 
     init {
         isResizable = false
         defaultCloseOperation = JFrame.EXIT_ON_CLOSE
         background = Color.white
 
-        add(SimulationView(simulation))
+        add(SimulationView(simulation, bus))
         pack()
         isVisible = true
     }
@@ -99,5 +129,6 @@ class SimulationFrame(val simulation: Simulation) : JFrame("Galactic Simulation"
 
 fun main(args: Array<String>){
     val sim = Simulation(systemCount=1000, planetsRange=0..10)
-    SimulationFrame(sim)
+    val bus = EventBus("Simulation Bus")
+    SimulationFrame(sim, bus)
 }
