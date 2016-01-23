17113b81d828ec2e689531de2f64e7e1f44985db
Nathanial Hartman <nathanial.hartman@gmail.com>
2016-01-23 14:45:28 -0800

    paint with refresh

--- src/galaxy_sim/vdom/swing.clj
+++ src/galaxy_sim/vdom/swing.clj
@@ -12,100 +12,102 @@
 (defmacro invoke-later [& args]
   `(SwingUtilities/invokeLater (fn [] ~@args)))
 
-(defn basic-transform []
-  (let [transform (AffineTransform. )]
+(defn basic-transform [sim]
+  (let [transform (AffineTransform. )
+        {:keys [scale translate]} (:transform sim)]
     (doto transform
-      (.translate 0.0 0.0)
-      (.scale 0.25 0.25))))
+      (.translate (:x translate) (:y translate))
+      (.scale (:x scale) (:y scale)))))
 
-(defn- create-dom-view [dom]
-  (proxy [JPanel] []
-    (paintComponent [^Graphics2D g]
-      (proxy-super paintComponent g)
-      (doto g
-        (.setRenderingHint RenderingHints/KEY_ANTIALIASING RenderingHints/VALUE_ANTIALIAS_ON)
-        (.setRenderingHint RenderingHints/KEY_RENDERING RenderingHints/VALUE_RENDER_QUALITY)
-        (.setColor (Color. 0 0 0))
-        (.setTransform (AffineTransform.))
-        (.fillRect 0 0 (.. this (getSize) width) (.. this (getSize) height))
-        (.setTransform (basic-transform)))
-      (doseq [element dom]
-        (painter/paint-element g element)))
-    (getPreferredSize  []
-      (Dimension. 1000 1000))))
+(defn- paint-sim [^Graphics2D g sim]
+  (let [{width :width, height :height} (:window sim)]
+    (doto g
+      (.setRenderingHint RenderingHints/KEY_ANTIALIASING RenderingHints/VALUE_ANTIALIAS_ON)
+      (.setRenderingHint RenderingHints/KEY_RENDERING RenderingHints/VALUE_RENDER_QUALITY)
+      (.setColor (Color. 0 0 0))
+      (.setTransform (AffineTransform.))
+      (.fillRect 0 0 width height)
+      (.setTransform (basic-transform sim)))
+    (doseq [element (:drawing sim)]
+      (painter/paint-element g element))))
 
-(defn- create-simulation-view []
-  (let [panel (JPanel. )
-        pane (JLayeredPane.)
-        domView (create-dom-view (:drawing @globals/sim-state))]
-    (doto domView
-      (.setBounds (Rectangle. 0 0 1000 1000)))
-    (doto pane
-      (.add domView 51)
-      (.setPreferredSize (Dimension. 1000 1000)))
-    (doto panel
-      (.setLayout (BorderLayout. ))
-      (.setBackground (Color/white))
-      (.add pane)
-      (.addMouseWheelListener
-        (proxy [MouseWheelListener] []
-          (mouseWheelMoved [e]
-            (.add event-queue {:event :mouse-wheel :zoom (* (.getWheelRotation e) (.getScrollAmount e))}))))
-      (.addMouseMotionListener
-        (proxy [MouseMotionAdapter] []
-          (mouseMoved [e]
-            (.add event-queue {:event :mouse-move :x (.getX e) :y (.getY e)}))
-          (mouseDragged [e]
-            (.add event-queue {:event :mouse-drag :x (.getX e) :y (.getY e)}))))
-      (.addMouseListener
-        (proxy [MouseInputAdapter] []
-          (mousePressed [e]
-            (.add event-queue {:event :mouse-down, :x (.getX e), :y (.getY e)}))
-          (mouseReleased [e]
-            (.add event-queue {:event :mouse-up, :x (.getX e), :y (.getY e)}))))
-      (.addComponentListener
-        (proxy [ComponentAdapter] []
-          (componentResized [e]
-            (let [width (.. panel size width)
-                  height (.. panel size height)]
-              (.add event-queue {
-                :event :window-resized
-                :width width
-                :height height})
-              (.setBounds domView (Rectangle. 0 0 width height))))
-          (componentMoved [e]
-            (let [width (.. panel size width)
-                  height (.. panel size height)]
-              (.add event-queue {
-                :event :window-moved
-                :width width
-              })
-              (.setBounds domView (Rectangle. 0 0 width height))))))
-      )))
+(defn start-paint-listener [dom-view]
+  (let [paint-loop (fn []
+                     (let [old-state (atom nil)]
+                       (loop []
+                         (let [new-state @globals/sim-state]
+                           (when (not= new-state @old-state)
+                             (swap! old-state (fn [_] new-state))
+                             (invoke-later
+                               (println "REPAINT THE THING")
+                               (.repaint dom-view))))
+                         (Thread/sleep 16)
+                         (recur))))]
+    (.start (Thread. paint-loop))))
 
-(defn paint-loop []
-  (let [old-state (atom nil)]
-    (loop []
-      (let [new-state @globals/sim-state]
-        (when (not= new-state @old-state)
-          (swap! old-state (fn [_] new-state))
-          ))
-      (Thread/sleep 16)
-      (recur))))
+ (defn- create-simulation-view []
+    (let [panel (JPanel.)
+          pane (JLayeredPane.)
+          domView (proxy [JPanel] []
+                    (paintComponent [^Graphics2D g]
+                      (proxy-super paintComponent g)
+                      (paint-sim g @globals/sim-state))
+                    (getPreferredSize []
+                      (Dimension. 1000 1000)))]
+      (start-paint-listener domView)
+      (doto domView
+        (.setBounds (Rectangle. 0 0 1000 1000)))
+      (doto pane
+        (.add domView 51)
+        (.setPreferredSize (Dimension. 1000 1000)))
+      (doto panel
+        (.setLayout (BorderLayout.))
+        (.setBackground (Color/white))
+        (.add pane)
+        (.addMouseWheelListener
+          (proxy [MouseWheelListener] []
+            (mouseWheelMoved [e]
+              (.add event-queue {:event :mouse-wheel :zoom (* (.getWheelRotation e) (.getScrollAmount e))}))))
+        (.addMouseMotionListener
+          (proxy [MouseMotionAdapter] []
+            (mouseMoved [e]
+              (.add event-queue {:event :mouse-move :x (.getX e) :y (.getY e)}))
+            (mouseDragged [e]
+              (.add event-queue {:event :mouse-drag :x (.getX e) :y (.getY e)}))))
+        (.addMouseListener
+          (proxy [MouseInputAdapter] []
+            (mousePressed [e]
+              (.add event-queue {:event :mouse-down, :x (.getX e), :y (.getY e)}))
+            (mouseReleased [e]
+              (.add event-queue {:event :mouse-up, :x (.getX e), :y (.getY e)}))))
+        (.addComponentListener
+          (proxy [ComponentAdapter] []
+            (componentResized [e]
+              (let [width (.. panel size width)
+                    height (.. panel size height)]
+                (.add event-queue {
+                                   :event  :window-resized
+                                   :width  width
+                                   :height height})
+                (.setBounds domView (Rectangle. 0 0 width height))))
+            (componentMoved [e]
+              (let [width (.. panel size width)
+                    height (.. panel size height)]
+                (.add event-queue {
+                                   :event :window-moved
+                                   :width width
+                                   })
+                (.setBounds domView (Rectangle. 0 0 width height))))))
+        )))
 
-
-(defn start-paint-listener []
-  (.start (Thread. paint-loop)))
-
-(defn start-app []
-  (invoke-later
-    (let [frame (JFrame. "Galactic Simulation")]
-      (doto frame
-        (.setResizable true)
-        (.setDefaultCloseOperation JFrame/EXIT_ON_CLOSE)
-        (.setBackground Color/white)
-        (.add (create-simulation-view))
-        (.pack)
-        (.show)))
-    (start-paint-listener)))
+  (defn start-app []
+    (invoke-later
+      (let [frame (JFrame. "Galactic Simulation")]
+        (doto frame
+          (.setResizable true)
+          (.setDefaultCloseOperation JFrame/EXIT_ON_CLOSE)
+          (.setBackground Color/white)
+          (.add (create-simulation-view))
+          (.pack)
+          (.show)))))
 
