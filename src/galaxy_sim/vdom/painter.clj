991fc505547e6bcf17b8a2316fef54e837926d97
Nathanial Hartman <nathanial.hartman@gmail.com>
2016-01-24 06:00:22 -0800

    preparing to add buffer

--- src/galaxy_sim/vdom/painter.clj
+++ src/galaxy_sim/vdom/painter.clj
@@ -5,10 +5,11 @@
 (defmulti paint-element (fn [_ element] (:type element)))
 
 (defmethod paint-element :ellipse [^Graphics2D graphics element]
-  (let [[^Integer red ^Integer green ^Integer blue] (:color element)
+  (let [[^Integer red ^Integer green ^Integer blue alpha?] (:color element)
+        ^Integer alpha (if (nil? alpha?) 255 alpha?)
         [a b c d] (:vertices element)
         shape (Ellipse2D$Double. a b c d)]
     (doto graphics
-      (.setColor (Color. red green blue)))
+      (.setColor (Color. red green blue alpha)))
     (when (:fill element)
       (.fill graphics shape))))
