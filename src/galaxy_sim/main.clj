eb023bd1541c47dfc2e8865a83ba0c6e910267a1
Nathan Hartman <nathanial.hartman@gmail.com>
2016-02-04 00:13:11 -0800

    simplified paint code

--- src/galaxy_sim/main.clj
+++ src/galaxy_sim/main.clj
@@ -11,8 +11,12 @@
            (javax.swing JPanel)
            (java.awt.image BufferedImage)))
 
-(defn- paint-tiles [^Graphics2D g transform tiles]
-  (let [scale (:scale transform)]
+(defn- paint-sim [^Graphics2D g]
+  (let [sim @globals/sim-state
+        transform (:transform sim)
+        scale (:scale transform)
+        tiles (tile-renderer/render-as-tiles transform (:window sim) (:drawing sim))]
+    (swing.core/set-graphic-defaults g)
     (doseq [tile tiles]
       (let [^BufferedImage image (:image tile)
             transform (painter/create-transform scale (:translate transform) (:translate tile))]
@@ -22,29 +26,10 @@
           (.setColor Color/red)
           (.drawRect 0 0 (dec tile-renderer/tile-width) (dec tile-renderer/tile-height)))))))
 
-(defn- paint-sim [^Graphics2D g]
-  (let [sim @globals/sim-state
-        transform (:transform sim)
-        tiles (tile-renderer/render-as-tiles transform (:window sim) (:drawing sim))]
-    (doto g
-      (.setRenderingHint RenderingHints/KEY_ANTIALIASING RenderingHints/VALUE_ANTIALIAS_ON)
-      (.setRenderingHint RenderingHints/KEY_RENDERING RenderingHints/VALUE_RENDER_QUALITY))
-    (paint-tiles g transform tiles)))
-
 (defn- should-repaint [old-state new-state]
   (or (not= (:drawing new-state) (:drawing old-state))
       (not= (:transform new-state) (:transform old-state))
-      (not= (:window new-state) (:window old-state))) )
-
-(defn redraw-listener [key reference old-state new-state]
-  (when (or (not= (:simulation old-state) (:simulation new-state)))
-    (send reference #(assoc %1 :drawing (simulation/draw (:simulation %1))))))
-
-(defn- repaint-listener [^JPanel canvas]
-  (fn [key reference old-state new-state]
-    (when (should-repaint old-state new-state)
-      (swing.core/invoke-later
-        (.repaint canvas)))))
+      (not= (:window new-state) (:window old-state))))
 
 (defn start-game []
   (pan-zoom/init)
@@ -52,11 +37,18 @@
   (let [sim (simulation/create)
         drawing (simulation/draw sim)]
     (send globals/sim-state assoc :simulation sim :drawing drawing)
-    (add-watch globals/sim-state :redraw redraw-listener)
+    (add-watch globals/sim-state :redraw
+               (fn [key reference old-state new-state]
+                 (when (or (not= (:simulation old-state) (:simulation new-state)))
+                   (send reference #(assoc %1 :drawing (simulation/draw (:simulation %1)))))))
     (swing.core/invoke-later
       (let [frame (swing.frame/create "Galactic Simulation")
             view (swing.core/create-custom-component paint-sim)]
-        (add-watch globals/sim-state :canvas-repaint (repaint-listener view))
+        (add-watch globals/sim-state :canvas-repaint
+                   (fn [key reference old-state new-state]
+                     (when (should-repaint old-state new-state)
+                       (swing.core/invoke-later
+                         (.repaint view)))))
         (.add frame view)))))
 
 (defn -main [& args]
